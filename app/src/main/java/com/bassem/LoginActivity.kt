f6074cf8a84ff77d1b3a604602fd222dd2c7f803
basemomar94 <basemomar2012@gmail.com>
2022-02-02 19:51:40 +0200

    t

---
+++ app/src/main/java/com/bassem/LoginActivity.kt
@@ -0,0 +1,107 @@
+package com.bassem
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import com.bassem.clinicdoctorapp.MainActivity
+import com.bassem.clinicdoctorapp.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var db: FirebaseFirestore
+    lateinit var mail: EditText
+    lateinit var password: EditText
+    lateinit var loginBu: Button
+    lateinit var loading: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mail = findViewById(R.id.mail_log)
+        password = findViewById(R.id.password_log)
+        loginBu = findViewById(R.id.confirmBu)
+        loading = findViewById(R.id.progressLogin)
+        checkLogin()
+        loginBu.setOnClickListener {
+            if (mail.text.isNotEmpty() && password.text.isNotEmpty()) {
+                checkDoctor()
+            }
+
+        }
+
+
+    }
+
+    fun checkDoctor() {
+        showLoading()
+        db = FirebaseFirestore.getInstance()
+        db.collection("doctors")
+            .document(mail?.text.toString().lowercase(Locale.getDefault()).trim())
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "please check your username",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    showButton()
+                } else {
+                    val firebaseKey = value?.getString("key")
+                    if (firebaseKey == password?.text.toString().lowercase(Locale.getDefault())
+                            .trim()
+                    ) {
+                        saveLogin()
+                        goToDashboard()
+                    } else {
+                        Snackbar.make(
+                            findViewById(android.R.id.content),
+                            "please check your password",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        showButton()
+
+                    }
+
+
+                }
+            }
+    }
+
+    fun goToDashboard() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun showLoading() {
+        loginBu.visibility = View.GONE
+        loading.visibility = View.VISIBLE
+    }
+
+    fun showButton() {
+        loginBu.visibility = View.VISIBLE
+        loading.visibility = View.GONE
+    }
+
+    fun saveLogin() {
+        val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putBoolean("key", true)
+        editor.apply()
+    }
+
+    fun checkLogin() {
+        val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val key = pref.getBoolean("key", false)
+        if (key) {
+            goToDashboard()
+        }
+
+    }
+}
--- app/src/main/java/com/bassem/clinicdoctorapp/schedule/Expand_visit.kt
+++ app/src/main/java/com/bassem/clinicdoctorapp/schedule/Expand_visit.kt
@@ -1,10 +1,11 @@
-package com.bassem.clinicdoctorapp
+package com.bassem.clinicdoctorapp.schedule
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bassem.clinicdoctorapp.R
 
 import com.bassem.clinicdoctorapp.databinding.VisitExpandBinding
 import com.google.firebase.firestore.FirebaseFirestore
